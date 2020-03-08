b62f7e5b4d45b3a6db4c4e0cdab83e9079a370e2
Peter Mooney <peter.mooney90@gmail.com>
2020-03-08 14:21:21 -0700

    Add apollo-client dependencies

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
 import LocationPicker from "./components/LocationPicker";
 import "./App.css";
 
+const client = new ApolloClient({ uri: "http://localhost:4000" });
+
 function App() {
   return (
-    <div className="App">
-      <main>
-        <LocationPicker />
-      </main>
-      <footer>
-        <a href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
-      </footer>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <main>
+          <LocationPicker />
+        </main>
+        <footer>
+          <a href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
+        </footer>
+      </div>
+    </ApolloProvider>
   );
 }
 
